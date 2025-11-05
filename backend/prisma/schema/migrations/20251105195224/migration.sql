f6267bb9ba186c7f766913d180a0001908410c22
Gobigan <gobigan.mathialahan@cashflowpositif.net>
2025-11-05 21:00:35 +0100

    feat(prisma): update token field length and add migration scripts for account and session models

---
+++ backend/prisma/schema/migrations/20251105195224/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE `accounts` MODIFY `accessToken` TEXT NULL,
+    MODIFY `refreshToken` TEXT NULL,
+    MODIFY `idToken` TEXT NULL;
---
+++ backend/prisma/schema/migrations/20251105195804/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `sessions` MODIFY `token` VARCHAR(2000) NOT NULL;
